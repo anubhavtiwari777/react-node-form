4cfb774a62a66c2ed1b34843c40ef5e6f7346fa8
anubhavtiwari777 <33908607+anubhavtiwari777@users.noreply.github.com>
2022-10-25 20:25:34 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import axios from "axios";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
